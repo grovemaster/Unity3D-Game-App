3d0cf9fd9d2ef7493c68896d6e805188b3e72cb6
jhall <grovemaster@hotmail.com>
2018-07-17 14:43:18 -0500

    GA-18  Tile implementer broken up for maintainability.

---
+++ Assets/Board Game App/Scripts/ECS/Implementor/Board/Tile/TileHighlightImpl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using Data.Enum;
+using ECS.Component.SharedComponent;
+using Svelto.ECS;
+using UnityEngine;
+
+namespace ECS.Implementor.Board.Tile
+{
+    class TileHighlightImpl : MonoBehaviour, IImplementor, IHighlight, IChangeColorComponent
+    {
+        public DispatchOnSet<bool> IsPressed { get; set; }
+
+        public bool IsHighlighted { get; set; }
+
+        public HashSet<HighlightState> CurrentColorStates { get; set; }
+
+        public bool PlayChangeColor
+        {
+            set
+            {
+                if (value)
+                {
+                    ChangeColor();
+                }
+            }
+        }
+
+        void Awake()
+        {
+            IsPressed = new DispatchOnSet<bool>(gameObject.GetInstanceID());
+            IsHighlighted = false;
+            CurrentColorStates = new HashSet<HighlightState>();
+        }
+
+        void OnMouseDown()
+        {
+            IsPressed.value = true;
+        }
+
+        private void ChangeColor()
+        {
+            var sprite = GetComponentInChildren<SpriteRenderer>();
+
+            bool hasBlackClick = CurrentColorStates.Contains(HighlightState.PLAYER_BLACK_CLICK_HIGHLIGHT);
+            bool hasWhiteClick = CurrentColorStates.Contains(HighlightState.PLAYER_WHITE_CLICK_HIGHLIGHT);
+
+            if (hasBlackClick && hasWhiteClick)
+            {
+                sprite.color = Color.yellow;
+            }
+            else if (hasBlackClick)
+            {
+                sprite.color = Color.magenta;
+            }
+            else if (hasWhiteClick)
+            {
+                sprite.color = Color.red;
+            }
+            else // HighlightState.DEFAULT or nothing
+            {
+                sprite.color = Color.gray;
+            }
+        }
+    }
+}
--- Assets/Board Game App/Scripts/ECS/Implementor/Board/Tile/TileImpl.cs
+++ Assets/Board Game App/Scripts/ECS/Implementor/Board/Tile/TileImpl.cs
@@ -2,72 +2,19 @@
 using Data.Enum;
 using ECS.Component.Board.Tile;
 using ECS.Component.SharedComponent;
-using Svelto.ECS;
 using UnityEngine;
 
 namespace ECS.Implementor.Board.Tile
 {
-    class TileImpl : MonoBehaviour, IImplementor, ITile, IHighlight, ILocation, IChangeColorComponent
+    class TileImpl : MonoBehaviour, IImplementor, ITile, ILocation
     {
-        public DispatchOnSet<bool> IsPressed { get; set; }
-
-        public bool IsHighlighted { get; set; }
-
         public Vector3 Location { get; set; }
 
         public int? PieceRefEntityId { get; set; }
 
-        public HashSet<HighlightState> CurrentColorStates { get; set; }
-
-        public bool PlayChangeColor
-        {
-            set
-            {
-                if (value)
-                {
-                    ChangeColor();
-                }
-            }
-        }
-
         void Awake()
         {
-            IsPressed = new DispatchOnSet<bool>(gameObject.GetInstanceID());
-            IsHighlighted = false;
-            CurrentColorStates = new HashSet<HighlightState>();
             // Location is set in Context
-        }
-
-        void OnMouseDown()
-        {
-            Debug.Log("Tile OnMouseDown " + Location.ToString());
-            IsPressed.value = true;
-        }
-
-        private void ChangeColor()
-        {
-            Debug.Log("Changing color of Tile " + Location.ToString());
-            var sprite = GetComponentInChildren<SpriteRenderer>();
-
-            bool hasBlackClick = CurrentColorStates.Contains(HighlightState.PLAYER_BLACK_CLICK_HIGHLIGHT);
-            bool hasWhiteClick = CurrentColorStates.Contains(HighlightState.PLAYER_WHITE_CLICK_HIGHLIGHT);
-
-            if (hasBlackClick && hasWhiteClick)
-            {
-                sprite.color = Color.yellow;
-            }
-            else if (hasBlackClick)
-            {
-                sprite.color = Color.magenta;
-            }
-            else if (hasWhiteClick)
-            {
-                sprite.color = Color.red;
-            }
-            else // HighlightState.DEFAULT or nothing
-            {
-                sprite.color = Color.gray;
-            }
         }
     }
 }
