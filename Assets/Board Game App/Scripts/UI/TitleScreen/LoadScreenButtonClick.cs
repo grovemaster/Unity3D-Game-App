de78b970735f0e8f5bdf3e1db088ccae17ad87be
grovemaster <40444524+grovemaster@users.noreply.github.com>
2018-09-15 10:15:47 -0500

    Removed unused scene, added rules scene for show player the rules of the game

---
+++ Assets/Board Game App/Scripts/UI/TitleScreen/LoadScreenButtonClick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI.TitleScreen
+{
+    public class LoadScreenButtonClick : MonoBehaviour
+    {
+        public void LoadNewGameScene()
+        {
+            SceneManager.LoadScene("BoardGame");
+        }
+
+        public void LoadRulesScene()
+        {
+            SceneManager.LoadScene("Rules");
+        }
+
+        public void ExitGame()
+        {
+#if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
